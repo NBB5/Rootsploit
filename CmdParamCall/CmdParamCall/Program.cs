70e1d024d86a9fdcf3df7deeac1b5875af32f453
NBB5 <98120328+NBB5@users.noreply.github.com>
2022-06-03 22:13:32 +0300

    Add files via upload

---
+++ CmdParamCall/CmdParamCall/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace CmdParamCall
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey currentUserKey = Registry.CurrentUser;
+            RegistryKey key = currentUserKey.OpenSubKey("Software\\Classes\\ms-settings\\shell\\open\\cmdparam");
+
+            string param = key.GetValue("cmdparam").ToString();
+
+            ProcessStartInfo p = new ProcessStartInfo("cmd.exe");
+            p.Arguments = param;
+            Process.Start(p);
+        }
+    }
+}
